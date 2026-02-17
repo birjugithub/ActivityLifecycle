3970aa27e5ad6002c65d7d4dd89807fa74927f1b
bky62 <kumarbrijkishor189@gmail.com>
2026-02-17 20:07:07 +0530

    This commit about activity lifecycle and when call it's method.

---
+++ app/src/main/java/com/activitylifecycle/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.activitylifecycle
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this@MainActivity,"Call onCreate method!", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Toast.makeText(this@MainActivity,"Call onStart method!", Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this@MainActivity,"Call onRestart method!", Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this@MainActivity,"Call onResume method!", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity,"Call onPause method!", Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this@MainActivity,"Call onStop method!", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this@MainActivity,"Call onDestroy method!", Toast.LENGTH_LONG).show()
+    }
+
+}
